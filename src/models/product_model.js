1f54318f7305482021041d2b0c70f515607f0413
DHuy <huynguyen07080401@gmail.com>
2023-12-11 11:30:26 +0700

    first commit

---
+++ src/models/product_model.js
@@ -0,0 +1,36 @@
+const { Schema , model } = require("mongoose")
+
+const ProductModel = new Schema({
+    name : {
+        type : String,
+    },
+    status : {
+        type : String,
+    },
+    slug :{
+        type : String,
+    },
+    ordering : {
+        type : Number,
+    },
+    price : {
+        type : Number,
+    },
+    content : {
+        type : String,
+    },
+    arrCheck : [
+        String,
+    ],
+    avatar : [
+        String,
+    ],
+    id_category : {
+        type : String,
+    },
+},{
+    timestamps : true
+})
+
+
+module.exports = model('products' , ProductModel)
